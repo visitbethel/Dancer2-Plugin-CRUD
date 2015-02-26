20df3bc7f4923a37fad6908e47b9a42385af1afb
visitbethel <visitbethel@gmail.com>
2015-02-25 20:06:47 -0500

    Lowercase and Any wildcard function.

--- Dancer2-Plugin-CRUD/lib/Dancer2/Plugin/Pagination.pm
+++ Dancer2-Plugin-CRUD/lib/Dancer2/Plugin/Pagination.pm
@@ -104,18 +104,19 @@ THe options are defined in the ng-grid on the controller.
 =cut
 
 sub get_lookups {
-  my ( $self, $store, $table, $map, $where, $columns ) = @_;
-  my $columm_map = $columns ? { columns => $columns } : $columns;
-  return &map_fields( $map, $store->resultset($table)->search( $where, $columm_map ) );
+	my ( $self, $store, $table, $map, $where, $columns ) = @_;
+	my $columm_map = $columns ? { columns => $columns } : $columns;
+	return &map_fields( $map,
+		$store->resultset($table)->search( $where, $columm_map ) );
 }
-
 
 =head
   ------------- mapper -------------  ------------- 
 =cut
+
 sub mapper {
-  my ( $self, $defs, @rs ) = @_;
-  return &map_fields( $defs, @rs );
+	my ( $self, $defs, @rs ) = @_;
+	return &map_fields( $defs, @rs );
 }
 
 =head
@@ -134,211 +135,261 @@ sub mapper {
     ]
   ------------- map field -------------  ------------- 
 =cut
+
 sub map_field {
-  my ( $k, $method, $base ) = @_;
-  my $value = undef;
+	my ( $k, $method, $base ) = @_;
+	my $value = undef;
 
-  if ( ref($method) eq '' ) {
-    $base = $base->$method if $base;
-    printf "      : %s->%s traverse\n", 'base', $method
-      if $DEBUG_RESULTSET
-        and $DEBUG_MAPPING;
-    return $base;
-  }
-  elsif ( ref($method) eq 'HASH' ) {
+	if ( ref($method) eq '' ) {
+		$base = $base->$method if $base;
+		printf "      : %s->%s traverse\n", 'base', $method
+		  if $DEBUG_RESULTSET
+			  and $DEBUG_MAPPING;
+		return $base;
+	}
+	elsif ( ref($method) eq 'HASH' ) {
 
-    # if there is a base find its path values.
-    if ($base) {
+		# if there is a base find its path values.
+		if ($base) {
 
-      # array ref, multiple values in one
-      my %bag = ();
-      foreach my $hashkeys ( keys %{$method} ) {
-        $bag{ $method->{$hashkeys} } = $base->$hashkeys;
-        printf "HASH : %s->%s baging ref(%s)\n", $method, $hashkeys, ref( $base->$hashkeys )
-          if $DEBUG_RESULTSET
-            and $DEBUG_MAPPING;
-      }
+			# array ref, multiple values in one
+			my %bag = ();
+			foreach my $hashkeys ( keys %{$method} ) {
+				$bag{ $method->{$hashkeys} } = $base->$hashkeys;
+				printf "HASH : %s->%s baging ref(%s)\n", $method, $hashkeys,
+				  ref( $base->$hashkeys )
+				  if $DEBUG_RESULTSET
+					  and $DEBUG_MAPPING;
+			}
 
-      #&logf(Dumper( \%bag ));
-      $value = \%bag;
-    }
-    else {
-      $value = undef;
-    }
-  }
-  else {
+			#&logf(Dumper( \%bag ));
+			$value = \%bag;
+		}
+		else {
+			$value = undef;
+		}
+	}
+	else {
 
-    # if there is a base find its path values.
-    if ($base) {
+		# if there is a base find its path values.
+		if ($base) {
 
-      # array ref, multiple values in one
-      my %bag = ();
-      foreach my $hashkeys ( @{$method} ) {
-        $bag{$hashkeys} = $base->$hashkeys;
-        printf "ARRAY: %s->%s baging ref(%s)\n", $method, $hashkeys, ref( $base->$hashkeys )
-          if $DEBUG_RESULTSET
-            and $DEBUG_MAPPING;
-      }
+			# array ref, multiple values in one
+			my %bag = ();
+			foreach my $hashkeys ( @{$method} ) {
+				$bag{$hashkeys} = $base->$hashkeys;
+				printf "ARRAY: %s->%s baging ref(%s)\n", $method, $hashkeys,
+				  ref( $base->$hashkeys )
+				  if $DEBUG_RESULTSET
+					  and $DEBUG_MAPPING;
+			}
 
-      #&logf(Dumper( \%bag ));
-      $value = \%bag;
-    }
-    else {
-      $value = undef;
-    }
-  }
+			#&logf(Dumper( \%bag ));
+			$value = \%bag;
+		}
+		else {
+			$value = undef;
+		}
+	}
 }
 
 =head
   ------------- pagination -------------  ------------- 
 =cut
+
 sub pagination {
-  my ( $self, $store, $table, $mapping, $subselect, $preselect, $calculated_sub ) = @_;
+	my ( $self, $store, $table, $mapping, $subselect, $preselect,
+		$calculated_sub )
+	  = @_;
 
-  my %params = $self->params;
-  &logf("PARAMS:" . Dumper( \%params ));
-  unless ($mapping) {
-    die( "missing mapping for " . $table );
-    return 1;
-  }
+	my %params = $self->params;
+	&logf( "PARAMS:" . Dumper( \%params ) );
+	unless ($mapping) {
+		die( "missing mapping for " . $table );
+		return 1;
+	}
 
-  # deserialize the filter, page and sort options.
-  my $pager  = $self->params->{'pager'} ? decode_json( $self->params->{'pager'}) : {};
-  my $filter = $self->params->{'filter'} ? decode_json( $self->params->{'filter'}) : {};
-  my $sort   =  $self->params->{'sort'} ? decode_json(  $self->params->{'sort'} ) : {};
+	# deserialize the filter, page and sort options.
+	my $pager =
+	  $self->params->{'pager'} ? decode_json( $self->params->{'pager'} ) : {};
+	my $filter =
+	  $self->params->{'filter'} ? decode_json( $self->params->{'filter'} ) : {};
+	my $sort =
+	  $self->params->{'sort'} ? decode_json( $self->params->{'sort'} ) : {};
 
-  my $limit         = $pager  && $pager->{'pageSize'}     || $DEFAULT_PAGE_SIZE;
-  my $pagenr        = $pager  && $pager->{'currentPage'}  || 1;
-  my $type          = $filter && $filter->{'matchType'}   || 'any';
-  my $text          = $filter && $filter->{'filterText'}  || '';
-  my $field         = $filter && $filter->{'matchFields'} || ['name'];
-  my $ignorecase    = $filter && $filter->{'ignorecase'}  || 1;
-  my $sortFields    = $sort   && $sort->{'field'};
-  my $sortDirection = $sort   && $sort->{'direction'}    || ['ASC'];
+	my $limit      = $pager  && $pager->{'pageSize'}     || $DEFAULT_PAGE_SIZE;
+	my $pagenr     = $pager  && $pager->{'currentPage'}  || 1;
+	my $type       = $filter && $filter->{'matchType'}   || 'any';
+	my $text       = $filter && $filter->{'filterText'}  || '';
+	my $field      = $filter && $filter->{'matchFields'} || ['name'];
+	my $ignorecase = $filter && $filter->{'ignorecase'}  || 1;
+	my $wildcard   = $filter && $filter->{'wildcard'}    || 1;
+	my $sortFields = $sort   && $sort->{'field'};
+	my $sortDirection = $sort && $sort->{'direction'} || ['ASC'];
 
-  # establish default filtering
-  my @column = ();
-  my %rev = reverse %{$mapping};
+	# establish default filtering
+	my @column = ();
+	my %rev    = reverse %{$mapping};
 
-  foreach my $expr (@$field) {
-    my ( $k, $dbfield ) = split /\./, $expr;    #/
-    printf "k=%s, field=%s  [%s]\n", $k, $dbfield ? $dbfield : 'NA', $expr;
-    if ($dbfield) {
-      my %rmap = reverse %{ $mapping->{$k} };
-      push @column, $rmap{$dbfield};
-    }
-    else {
-      push @column, $rev{$k};
-    }
-  }
+	foreach my $expr (@$field) {
+		my ( $k, $dbfield ) = split /\./, $expr;    #/
+		printf "k=%s, field=%s  [%s]\n", $k, $dbfield ? $dbfield : 'NA', $expr;
+		if ($dbfield) {
+			my %rmap = reverse %{ $mapping->{$k} };
+			push @column, $rmap{$dbfield};
+		}
+		else {
+			push @column, $rev{$k};
+		}
+	}
 
+	#
+	#  my @column = keys %column;
+	#die Dumper($field) . " and " . Dumper(\@column);
+	#
+	my %where = $preselect ? %{$preselect} : ();
+	if ( $text ne '' ) {
+		if ( $type =~ /all/ ) {
+			foreach my $f (@column) {
+				next unless $f;
+				if ($ignorecase) {
+					if ($wildcard) {
+						$where{"LOWER(me.$f)"}{'-like'} = '%' . lc $text . '%';
+					}
+					else {
+						$where{"LOWER(me.$f)"}{'-like'} = lc $text . '%';
+					}
+				}
+				else {
+					if ($wildcard) {
+						$where{"me.$f"}{'-like'} = '%' . $text . '%';
+					}
+					else {
+						$where{"me.$f"}{'-like'} = $text . '%';
+					}
+				}
 
-  #
-  #  my @column = keys %column;
-  #die Dumper($field) . " and " . Dumper(\@column);
-  #
-  my %where = $preselect ? %{$preselect} : ();
-  if ( $text ne '' ) {
-    if ( $type =~ /all/ ) {
-      foreach my $f (@column) {
-        next unless $f;
-        if ($ignorecase) {
-          $where{"LOWER(me.$f)"}{'-like'} = lc $text  . '%';
-        }
-        else {
-          $where{"me.$f"}{'-like'} = $text . '%';
-        }
+			}
+		}
+		else {
+			&logf( "<><<<<<<<<<<<<<<<<<"
+				  . Dumper( $text, \@column, \@$field, $mapping ) )
+			  if $DEBUG_RESULTSET;
+			if ( scalar @column > 1 ) {
+				foreach my $f (@column) {
+					next unless $f;
+					if ($ignorecase) {
+						if ($wildcard) {
+							push @{ $where{'-or'} },
+							  { "LOWER(me.$f)" =>
+								  { '-like' => '%' . lc $text . '%' } };
+						}
+						else {
+							push @{ $where{'-or'} },
+							  { "LOWER(me.$f)" => { '-like' => lc $text . '%' }
+							  };
+						}
+					}
+					else {
+						if ($wildcard) {
+							push @{ $where{'-or'} },
+							  { "me.$f" => { '-like' => '%' . $text . '%' } };
 
-      }
-    }
-    else {
-      &logf("<><<<<<<<<<<<<<<<<<" . Dumper( $text, \@column, \@$field, $mapping )) if $DEBUG_RESULTSET;
-      if ( scalar @column > 1 ) {
-        foreach my $f (@column) {
-          next unless $f;
-          if ($ignorecase) {
-            push @{ $where{'-or'} }, { "LOWER(me.$f)" => { '-like' =>  $text  . '%' } };
-          }
-          else {
-            push @{ $where{'-or'} }, { "me.$f" => { '-like' => $text . '%' } };
-          }
+						}
+						else {
+							push @{ $where{'-or'} },
+							  { "me.$f" => { '-like' => $text . '%' } };
 
-        }
-      }
-      else {
-        foreach my $f (@column) {
-          next unless $f;
-          if ($ignorecase) {
-            $where{"LOWER(me.$f)"}{'-like'} = lc $text . '%';
-          }
-          else {
-            $where{"me.$f"}{'-like'} = $text . '%';
+						}
+					}
 
-          }
-        }
-      }
-    }
-  }
-  my @order = ();
+				}
+			}
+			else {
+				foreach my $f (@column) {
+					next unless $f;
+					if ($ignorecase) {
+						if ($wildcard) {
+							$where{"LOWER(me.$f)"}{'-like'} =
+							  '%' . lc $text . '%';
+						}
+						else {
+							$where{"LOWER(me.$f)"}{'-like'} = lc $text . '%';
+						}
+					}
+					else {
+						if ($wildcard) {
+							$where{"me.$f"}{'-like'} = '%' . $text . '%';
+						}
+						else {
+							$where{"me.$f"}{'-like'} = $text . '%';
+						}
 
-  #@order = map { $mapping->{$_} . ' ASC' } @$sortFields if $sortFields;
-  foreach my $field (@{$sortFields}) {
-  	if ($mapping->{$rev{$field}}) {
-  		my $fld = sprintf "%s %s", $rev{$field}, $sortDirection->[0];
-  		push @order, $fld;
-  	}
-  }
-  print Dumper(">>>sorting:", $sort, $sortFields,  \@order);
+					}
+				}
+			}
+		}
+	}
+	my @order = ();
 
-  my %options = (
-    page     => $pagenr,
-    rows     => $limit,
-    order_by => \@order
+	#@order = map { $mapping->{$_} . ' ASC' } @$sortFields if $sortFields;
+	foreach my $field ( @{$sortFields} ) {
+		if ( $mapping->{ $rev{$field} } ) {
+			my $fld = sprintf "%s %s", $rev{$field}, $sortDirection->[0];
+			push @order, $fld;
+		}
+	}
+	print Dumper( ">>>sorting:", $sort, $sortFields, \@order );
 
-  );
-  if ( $subselect and ref($subselect) eq 'HASH' ) {
-    %options = ( %options, %$subselect );
-  }
-  &logf("OPTIONS:" . Dumper( \%options ));
-  &logf("WHERE  : " . Dumper( \%where ));
-  &logf("ORDER  : " . Dumper( \@order ));
-  my @rs = $store->resultset($table)->search( \%where, \%options );
-  my $count = $store->resultset($table)->search( \%where )->count;
+	my %options = (
+		page     => $pagenr,
+		rows     => $limit,
+		order_by => \@order
 
-  my $result = &map_fields( $mapping, @rs, $calculated_sub );
-  &logf("return " . scalar @$result . " of " . $count . " records.\n");
-  #&logf(Dumper($result));
-  return { 'totalItems' => $count, 'items' => $result };
+	);
+	if ( $subselect and ref($subselect) eq 'HASH' ) {
+		%options = ( %options, %$subselect );
+	}
+	&logf( "OPTIONS:" . Dumper( \%options ) );
+	&logf( "WHERE  : " . Dumper( \%where ) );
+	&logf( "ORDER  : " . Dumper( \@order ) );
+	my @rs = $store->resultset($table)->search( \%where, \%options );
+	my $count = $store->resultset($table)->search( \%where )->count;
+
+	my $result = &map_fields( $mapping, @rs, $calculated_sub );
+	&logf( "return " . scalar @$result . " of " . $count . " records.\n" );
+
+	#&logf(Dumper($result));
+	return { 'totalItems' => $count, 'items' => $result };
 }
-
 
 =head
   ------------- merge pages / union -------------  ------------- 
 =cut
+
 sub merge_pagers {
-  my %resultset = ( 'totalItems'=>0, 'items'=>[]);
-  foreach my $set (@_) {
-    if ($set and ref($set) eq 'HASH' and $set->{'totalItems'}) {
-      $resultset{'totalItems'} += $set->{'totalItems'};
-      my @list = @{$set->{'items'}};
-      print "\n>>>>>>>>>>>>>", ref($resultset{'items'});
-      my @totallist = @{$resultset{'items'}};
-      @list = (@list, @totallist);
-      $resultset{'items'} = \@list;
-    }
-  }
-  return \%resultset;
+	my %resultset = ( 'totalItems' => 0, 'items' => [] );
+	foreach my $set (@_) {
+		if ( $set and ref($set) eq 'HASH' and $set->{'totalItems'} ) {
+			$resultset{'totalItems'} += $set->{'totalItems'};
+			my @list = @{ $set->{'items'} };
+			print "\n>>>>>>>>>>>>>", ref( $resultset{'items'} );
+			my @totallist = @{ $resultset{'items'} };
+			@list = ( @list, @totallist );
+			$resultset{'items'} = \@list;
+		}
+	}
+	return \%resultset;
 }
 
 sub logf {
-  printf @_ if $DEBUG_MAPPING;
+	printf @_ if $DEBUG_MAPPING;
 }
 
-
-register get_lookups => \&get_lookups;
-register pager       => \&pagination;
+register get_lookups  => \&get_lookups;
+register pager        => \&pagination;
 register merge_pagers => \&merge_pagers;
-register mapper      => \&mapper;
+register mapper       => \&mapper;
 register_plugin for_versions => [ 1, 2 ];
 
 1;
